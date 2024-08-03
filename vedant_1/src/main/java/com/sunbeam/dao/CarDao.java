b96ee4d19ad80e2060ad9cdc4fe788af84924e7c
Amit-Raskar <amitraskar09@gmail.com>
2024-08-03 20:10:15 +0530

    signin,register,getcompany,getmodel,getvariant api

--- vedant_1/src/main/java/com/sunbeam/dao/CarDao.java
+++
@@ -1,8 +0,0 @@
-package com.sunbeam.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sunbeam.entities.Car;
-
-public interface CarDao extends JpaRepository<Car, Long> {
-
-}
