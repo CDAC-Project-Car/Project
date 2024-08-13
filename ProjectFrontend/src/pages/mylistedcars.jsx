af99ea7a6f53b28c76cfbd68a7fb235c61f1ea8e
Mishkeel-J <mishkeeljambhulkar09@gmail.com>
2024-08-13 14:31:00 +0530

    UI mylistedCars & service car.js

--- ProjectFrontend/src/pages/mylistedcars.jsx
+++ ProjectFrontend/src/pages/mylistedcars.jsx
@@ -1,92 +1,90 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-function MyCarList ()
-{
+import { useEffect, useState } from "react";
+import { myListedCarsApi } from "../services/car";
+import axios from "axios";
+
+function MyCarList() {
+
+    const [carData, setCarData] = useState([]);
+
+    const load = async () => {
+        
+        //TO DO - Get ID dynamically from somewhere else, for now using static id
+
+        const result = await myListedCarsApi(4);
+        
+        setCarData(result)
+    };
+
+    useEffect(() => {
+        load()
+    }, []);
+
     return <div>
+
         <Navbar />
         <div className="container">
-        <h3 style={{fontWeight: 'bold'}}><center>Listed Cars</center></h3>
+            <h3 className="text-center mb-4 mt-4" style={{ fontWeight: 'bold' }}>Listed Cars</h3>
+
             <div className="row mt-5">
-            <div className="col-2"></div>
-            <div className="col-8"  >
+                <div className="col-2"></div>
 
-               <center>
-               <table className="table table-striped mb-5">
-                    <thead>
-                        <th>Sr No.</th>
-                        <th>Car Name</th>
-                        <th>Model</th>
-                        <th>Price</th>
-                        <th>Listing Date</th>
-                        <th>Sell Status</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Brezza</td>
-                            <td>VXI</td>
-                            <td>1029000</td>
-                            <td>29/02/2016</td>
-                            <td>Not sold</td>
-                            <td>
-                                <button className="btn btn-outline-primary me-3">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                                </button>
-                                <button className="btn btn-outline-danger">
-                                <i class="fa-solid fa-trash"></i>
-                                </button>
+                <div className="col-8">
+                    <center>
+                        <table className="table table-striped md-5">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th>Company</th>
+                                    <th>Model</th>
+                                    <th>Variant</th>
+                                    <th>Selling Price</th>
+                                    <th>Listing Date</th>
+                                    <th>Car Status</th>
+                                    <th><center>Action</center></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    carData.map((car, index) => (
+                                        <tr key={index}>
+                                            <td>{car.carModelCompany}</td>
+                                            <td>{car.modelName}</td>
+                                            <td>{car.carSeriesName}</td>
+                                            <td>Rs. {car.carSellingPrice.toFixed(2)} /-</td>
+                                            {/* <td>{new Date(car.carListingDate).toLocaleDateString()}</td> */}
+                                            <td>{car.carListingDate}</td>
+                                            <td>{car.carStatus ? "Available" : "Sold"}</td>
+                                            <td>
+                                                <button className="btn btn-outline-primary me-3"
 
-                            </td>
-                        </tr>
+                                                    //BUTTON APIS REMAINING
 
-                        <tr>
-                            <td>2</td>
-                            <td>Harrier</td>
-                            <td>XM</td>
-                            <td>2029000</td>
-                            <td>29/02/2020</td>
-                            <td>Sold</td>
-                            <td>
-                                <button className="btn btn-outline-primary me-3">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                                </button>
-                                <button className="btn btn-outline-danger">
-                                <i class="fa-solid fa-trash"></i>
-                                </button>
+                                                    // onClick={}
+                                                    >
+                                                    <i class="fa-regular fa-pen-to-square"></i>
+                                                </button>
+                                                <button className="btn btn-outline-danger">
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </button>
 
-                            </td>
-                        </tr>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
 
-                        <tr>
-                            <td>3</td>
-                            <td>Thar</td>
-                            <td>XM</td>
-                            <td>2029000</td>
-                            <td>29/02/2020</td>
-                            <td>Sold</td>
-                            <td>
-                                <button className="btn btn-outline-primary me-3">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                                </button>
-                                <button className="btn btn-outline-danger">
-                                <i class="fa-solid fa-trash"></i>
-                                </button>
+                            </tbody>
+                        </table>
+                    </center>
+                </div>
 
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-               </center>
-
+                <div className="col-2"></div>
             </div>
-            <div className="col-2"></div>
-            </div>
-           
 
         </div>
         <Footer />
 
     </div>
 }
+
 export default MyCarList;
