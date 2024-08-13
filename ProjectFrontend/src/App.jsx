a37cc6ab85450f57c038b1e113da979d89ce70ce
Yash <yashkhadse9158@gmail.com>
2024-08-13 13:57:38 +0530

    minor changes and location modal

--- ProjectFrontend/src/App.jsx
+++ ProjectFrontend/src/App.jsx
@@ -1,37 +1,104 @@
-import './App.css';
-import { Route,Routes } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Sell from "./pages/sell";
 import Buy from "./pages/buy";
-import Compare from './pages/compare';
-import Login from './pages/login';
-import Register from './pages/register';
-import { ToastContainer, toast } from 'react-toastify';
-import Cart from './pages/cart';
-import MyCarList from './pages/mylistedcars';
-import MyOrders from './pages/myorders';
-import CompareRender from './pages/compareRender';
-
+import Compare from "./pages/compare";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { ToastContainer } from "react-toastify";
+import Cart from "./pages/cart";
+import MyCarList from "./pages/mylistedcars";
+import MyOrders from "./pages/myorders";
+import CompareRender from "./pages/compareRender";
+import { LocationProvider } from "./data/LocationContext";
 
 function App() {
-  return (<div>
-        <Routes>
-        <Route index element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/buy" element={<Buy/>}/>
-        <Route path="/sell" element={<Sell/>}/>
-        <Route path="/compare" element={<Compare/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/myorders" element={<MyOrders/>}/>
-        <Route path="/mylistedcars" element={<MyCarList/>}/>
-        <Route path="/compareRender" element={<CompareRender/>}/>
-        </Routes>
-        <ToastContainer />
+  return (
 
-  </div>
-  
+    /* 
+    
+    wrap your page component like this for locaion (if time permits will later reduce this)
+    <LocationProvider>
+              <Home />
+            </LocationProvider> */
+    <div>
+      <Routes>
+        {/* Routes that need LocationProvider */}
+        <Route
+          path="/home"
+          element={
+            <LocationProvider>
+              <Home />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/buy"
+          element={
+            <LocationProvider>
+              <Buy />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/sell"
+          element={
+            <LocationProvider>
+              <Sell />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/compare"
+          element={
+            <LocationProvider>
+              <Compare />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <LocationProvider>
+              <Cart />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/myorders"
+          element={
+            <LocationProvider>
+              <MyOrders />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/mylistedcars"
+          element={
+            <LocationProvider>
+              <MyCarList />
+            </LocationProvider>
+          }
+        />
+        <Route
+          path="/compareRender"
+          element={
+            <LocationProvider>
+              <CompareRender />
+            </LocationProvider>
+          }
+        />
+
+        {/* Routes that don't need LocationProvider */}
+        <Route index element={ <LocationProvider>
+              <Home />
+            </LocationProvider>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   );
 }
 
